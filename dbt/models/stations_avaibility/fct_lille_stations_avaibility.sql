18f8e20d890ceee78f3c3b4a3d6c23bc9123f0d6
Syrine Radhouane <syrine.radhouane394@gmail.com>
2025-02-26 10:53:01 +0000

    fix lille fct table

--- dbt/models/stations_avaibility/fct_lille_stations_avaibility.sql
+++ dbt/models/stations_avaibility/fct_lille_stations_avaibility.sql
@@ -8,6 +8,8 @@ final as (
     select
         station_id
         m_bikes_count,
+        null as e_bikes_count,
+        m_bikes_count as bikes_count,
         available_docks_count,
         last_reported_at,
         GCS_loaded_at
